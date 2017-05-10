01654026b865b7773667c1fd4bf81e31b0f2fc6c
Leslie A. Lopez <lesashley56@gmail.com>
2017-05-10 12:46:23 -0500

    Agregando hover en enlaces

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
-/*
- * Archivo principal de JS
- */
+var endScroll = 0;
+window.addEventListener("scroll", function () {
+  var nav = document.getElementById("js-nav");
+  var scroll = window.pageYOffset || document.body.scrollTop;
+  if (scroll > endScroll) {
+    nav.classList.add("nav-size");
+  }
+  else{
+    nav.classList.remove("nav-size");
+  }
+})
